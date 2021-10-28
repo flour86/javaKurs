7b6460e1aa8fafdcd054b9b36e2f16cab7cec267
kurs <r.brzychczy@zstio3.pl>
2021-10-28 17:08:18 +0200

    first commit

---
+++ src/main/java/pl/teslachorzow/apierwszyprogram/Main01.java
@@ -0,0 +1,9 @@
+package pl.teslachorzow.apierwszyprogram;
+
+public class Main01 {
+    //psvm
+    public static void main(String[] args) {
+        //sout
+        System.out.println("moj pierwszy program w java");
+    }
+}
